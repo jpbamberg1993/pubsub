5dd50e9b9629c7c418d8b53f8037b9489759b310
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-17 12:00:56 -0500

    use middleware

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+// returns middleware
+app.use(express.json());
+
+app.post('/', function(req, res){
+	res.send('hello world');
+});
